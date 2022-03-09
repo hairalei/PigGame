902bb3e7e0e1e4052503a234c760315b0265206d
Haira Lei <darhaii13@gmail.com>
2022-03-09 20:55:57 +0800

    Add the variables and some functions for the game logic

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 'use strict';
+
+const btnNewGame = document.querySelector('.btn--new');
+const btnRollDice = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const scoreP1Txt = document.querySelector('#score--0');
+const scoreP2Txt = document.querySelector('#score--1');
+const currScoreP1Txt = document.querySelector('#current--0');
+const currScoreP2Txt = document.querySelector('#current--1');
+const diceImg = document.querySelector('.dice');
+
+let dice = 0;
+let scoreP1 = 0;
+let scoreP2 = 0;
+let currScoreP1 = 0;
+let currScoreP2 = 0;
+let currPlayer = 1;
+
+function rollDice() {
+  dice = Math.floor(Math.random() * 6) + 1;
+  diceImg.src = `images/dice-${dice}.png`;
+  return dice;
+}
+
+function countScore() {
+  currScoreP1 += dice;
+  console.log(currScoreP1);
+  currScoreP1Txt.textContent = currScoreP1;
+}
+
+function holdScore() {
+  scoreP1 += currScoreP1;
+  scoreP1Txt.textContent = scoreP1;
+  currScoreP1 = 0;
+  currScoreP1Txt.textContent = currScoreP1;
+}
+
+btnRollDice.addEventListener('click', () => {
+  rollDice();
+  countScore();
+});
+
+btnHold.addEventListener('click', holdScore);
+
+function changePlayer(player) {
+  currPlayer === 1 ? (currPlayer = 1) : (currPlayer = 2);
+}
