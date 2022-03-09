5ac94c303a4c4e12c99677065397e201d9ea0ce5
Haira Lei <darhaii13@gmail.com>
2022-03-09 22:08:23 +0800

    The game is now working correctly

--- script.js
+++ script.js
@@ -7,41 +7,80 @@ const scoreP1Txt = document.querySelector('#score--0');
 const scoreP2Txt = document.querySelector('#score--1');
 const currScoreP1Txt = document.querySelector('#current--0');
 const currScoreP2Txt = document.querySelector('#current--1');
+const player1 = document.querySelector('.player--0');
+const player2 = document.querySelector('.player--1');
 const diceImg = document.querySelector('.dice');
 
 let dice = 0;
 let scoreP1 = 0;
 let scoreP2 = 0;
-let currScoreP1 = 0;
-let currScoreP2 = 0;
+let currScore = 0;
 let currPlayer = 1;
+let score = scoreP1;
+let scoreLabel;
+let currScoreLabel;
 
 function rollDice() {
   dice = Math.floor(Math.random() * 6) + 1;
   diceImg.src = `images/dice-${dice}.png`;
-  return dice;
-}
 
-function countScore() {
-  currScoreP1 += dice;
-  console.log(currScoreP1);
-  currScoreP1Txt.textContent = currScoreP1;
+  if (dice === 1) {
+    currScore = 0;
+    currScoreLabel.textContent = currScore;
+    switchPlayer();
+    setPlayer();
+  } else {
+    currScore += dice;
+    currScoreLabel.textContent = currScore;
+  }
 }
 
 function holdScore() {
-  scoreP1 += currScoreP1;
-  scoreP1Txt.textContent = scoreP1;
-  currScoreP1 = 0;
-  currScoreP1Txt.textContent = currScoreP1;
+  score += currScore;
+  scoreLabel.textContent = score;
+  currScore = 0;
+  console.log(`score: ${score}`);
+
+  console.log(`currscore: ${currScore}`);
+  currScoreLabel.textContent = currScore;
+
+  switchPlayer();
+
+  setPlayer();
 }
 
 btnRollDice.addEventListener('click', () => {
   rollDice();
-  countScore();
+  console.log(dice);
+  console.log(`currscore: ${currScore}`);
 });
 
 btnHold.addEventListener('click', holdScore);
 
-function changePlayer(player) {
-  currPlayer === 1 ? (currPlayer = 1) : (currPlayer = 2);
+function switchPlayer() {
+  if (currPlayer === 1) {
+    currPlayer = 2;
+    scoreP1 = score;
+  } else {
+    currPlayer = 1;
+    scoreP2 = score;
+  }
 }
+
+function setPlayer() {
+  if (currPlayer === 1) {
+    score = scoreP1;
+    scoreLabel = scoreP1Txt;
+    currScoreLabel = currScoreP1Txt;
+    player1.classList.add('player--active');
+    player2.classList.remove('player--active');
+  } else if (currPlayer === 2) {
+    score = scoreP2;
+    scoreLabel = scoreP2Txt;
+    currScoreLabel = currScoreP2Txt;
+    player1.classList.remove('player--active');
+    player2.classList.add('player--active');
+  }
+}
+
+setPlayer();
